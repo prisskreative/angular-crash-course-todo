799953f1c074a72a879dfe8a947adb7ee9d567f2
Priscilla <priss@prisskreative.com>
2019-08-27 12:46:54 -0500

    Create app with mock data

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      { 
+        id: 1,
+        title: 'Todo one',
+        completed: false
+      },
+      { 
+        id: 2,
+        title: 'Todo two',
+        completed: true
+      },
+      { 
+        id: 3,
+        title: 'Todo three',
+        completed: false
+      }
+    ]
+  }
+
+}
